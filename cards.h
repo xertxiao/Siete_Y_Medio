987090afe4ec530a80b54c0a53f20b65ee865379
Guorui Xiao <guoruixiao@s-164-67-98-139.resnet.ucla.edu>
2017-10-08 14:45:16 -0700

    add two enum to count numbers and items

--- cards.h
+++ cards.h
@@ -3,6 +3,12 @@
 #include <fstream>
 
 #ifndef CARDS_H_
-#define CARDS_H_
+#define CARDS_H
+
+enum suit_t {OROS, COPAS, ESPADAS, BASTOS};
+
+enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA = 9, CABALLO = 10, REY = 11};
+
+
 
 
