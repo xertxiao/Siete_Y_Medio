d9842d9f54642e32516dea4f12c0573bee0b1b71
Guorui Xiao <guoruixiao@GuoruideMacBook-Pro.local>
2017-10-08 18:27:51 -0700

    use switch method to implement card getrank, getsuit functions

--- cards.cpp
+++ cards.cpp
@@ -99,15 +99,66 @@ string Card::get_spanish_rank() const {
 // Accessor: returns a string with the suit of the card in English 
 // This is just a stub! Modify it to your liking.
 string Card::get_english_suit() const { 
-
-   return "";
+   string suitName;
+   switch (suit) {
+      case OROS: 
+         suitName = "Gold"; 
+	 break;
+      case COPAS: 
+         suitName = "Cups"; 
+	 break; 
+      case ESPADAS: 
+         suitName = "Swords"; 
+	 break;
+      case BASTOS: 
+         suitName = "Clubs"; 
+	 break; 
+      default: break;
+   }
+   return suitName;
 }
 
 // Accessor: returns a string with the rank of the card in English 
 // This is just a stub! Modify it to your liking.
 string Card::get_english_rank() const { 
-   return "";
-}
+   string rankName;
+   switch (rank) {
+      case AS:
+         rankName = "Ace"; 
+	 break; 
+      case DOS: 
+         rankName = "Two"; 
+	 break; 
+      case TRES: 
+         rankName = "Three"; 
+	 break; 
+      case CUATRO: 
+         rankName = "Four"; 
+	 break; 
+      case CINCO: 
+         rankName = "Five"; 
+	 break; 
+      case SEIS: 
+         rankName = "Six"; 
+	 break; 
+      case SIETE: 
+         rankName = "Seven"; 
+	 break; 
+      case SOTA: 
+         rankName = "Sota"; 
+	 break; 
+      case CABALLO: 
+         rankName = "Horse"; 
+	 break; 
+      case REY: 
+         rankName = "King"; 
+	 break; 
+      default: break;
+   }
+   return rankName;
+} 
+   
+
 
 
 
