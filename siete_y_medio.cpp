2fee2e58ae2dfdda31af8906785516b7611b8a34
Guorui Xiao <guoruixiao@GuoruideMacBook-Pro.local>
2017-10-10 21:21:01 -0700

    first create the main cpp file, havent implemented yet

---
+++ siete_y_medio.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
+
+int main(){
+	
+
+
+
+
+
+
+
+
+	return 0;
+}
